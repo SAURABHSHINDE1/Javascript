75b267a50e4e69de32a920246d3e78cb620f6309
SAURABHSHINDE1 <saurabhshinde779845@gmail.com>
2025-11-15 17:25:22 +0530

    day4

---
+++ functions/script.js
@@ -0,0 +1,56 @@
+
+
+// function greet(){
+
+// }
+
+// function key word
+
+// greet function name 
+
+// () function parameter
+
+// {} function body
+
+// greet()  function call
+
+
+// function mult(a,b){
+
+//     console.log(a*b)
+
+// }
+
+// mult(20 , 56)
+
+// mult(30, 30)
+
+// mult(67, 90)
+
+
+// function divide(d,g){
+
+//     console.log(d/g)
+
+// }
+
+// divide(30,2)
+
+
+// function greet(name){
+
+//     alert("hello " + name)  
+
+// }
+
+// greet("xyz")
+
+
+// const user = ()=>{
+ 
+//     alert("hello user!!")
+// }
+
+// user()
+
+
