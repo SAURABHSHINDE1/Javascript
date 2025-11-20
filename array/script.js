9dd9684c1e5e92fd6d0c1fe752bc7bd51892b1ed
SAURABHSHINDE1 <saurabhshinde779845@gmail.com>
2025-11-20 18:18:13 +0530

    day5

---
+++ array/script.js
@@ -0,0 +1,23 @@
+// let arr = [45,67,89,45,69,34,92,90,43]
+
+
+// arr.splice(3,3,77)
+
+// console.log(arr)
+
+
+// slice splice
+// includes
+// let result = arr.slice(0,5)
+
+// console.log(result)
+
+// let data =["om" ,"ram" ,"xyz" ,"rohit"]
+
+// let result = data.includes("abc")
+
+// console.log(result)
+
+let arr=[1,2,3,4,5,6,7,8,9,10]
+
+console.log(arr) 
---
+++ loops/loops.js
@@ -0,0 +1,32 @@
+// for(let i = 10; i > 0 ; i--){
+
+//     console.log(i)
+
+// }
+
+
+
+// let arr = [10,45,78,66,77,90,34,24,38,57,99,22,11]
+
+// for(let i = 0; i< arr.length ; i++){
+//     console.log(arr[i] )
+// }
+
+
+// let i = 10;
+
+// while(i > 0){
+//     console.log(i);
+//     i--;
+// }
+
+// let i = 1
+
+// do{
+//     console.log(i)
+//     i++;
+// }while(i < 0)
+
+
+i++
+++i
