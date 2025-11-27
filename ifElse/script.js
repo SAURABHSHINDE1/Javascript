7de27da76481789755c054fe07a3158ff7f54dcb
SAURABHSHINDE1 <saurabhshinde779845@gmail.com>
2025-11-27 17:00:05 +0530

    day 7

---
+++ ifElse/script.js
@@ -0,0 +1,90 @@
+// let marks = 20
+
+// if(marks >= 90){
+
+//     console.log("O grade !!")
+
+// }
+// else if(marks >= 70){
+
+//     console.log("A grade !!")
+
+// }
+// else if(marks >= 50){
+
+//     console.log("B grade !!")
+
+// }
+// else if(marks >= 40){
+
+//     console.log("c grade !!")
+// }
+// else{
+
+//     console.log("You are fail")
+// }
+
+
+// let age = 2
+
+// if(age >= 60){
+
+//     console.log("You are elder")
+
+// }
+// else if(age >= 30){
+
+//     console.log("You are middle")
+
+// }
+// else if(age >= 18){
+
+//     console.log("you are younger")
+
+// }
+// else if(age >= 6){
+
+//     console.log("you are chlid ")
+
+// }
+// else{
+//     console.log("invalid")
+// }
+
+
+let day = "8"
+
+switch(day){
+
+    case "1":
+        console.log("monday");
+        break;
+
+    case "2":
+        console.log("Tuesday");
+        break;
+
+    case "3":
+        console.log("wedensday");
+        break;
+
+    case "4":
+        console.log("Thursday");
+        break;
+
+    case "5":
+        console.log("Friday");
+        break;
+
+    case "6":
+        console.log("Saturday");
+        break;
+
+    case "7":
+        console.log("Sunday");
+        break;
+
+    default:
+        console.log("invalid data")
+}
+
---
+++ operators/script.js
@@ -0,0 +1,5 @@
+let num = 5
+
+let num2 =5
+
+console.log(num != num2)
