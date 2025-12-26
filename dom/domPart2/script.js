a690a1dfd70ba31ae4be9dd0f586fd12c4d13e36
SAURABHSHINDE1 <saurabhshinde779845@gmail.com>
2025-12-26 17:11:12 +0530

    26 dec 2025

--- dom/domPart2/script.js
+++ dom/domPart2/script.js
@@ -63,10 +63,116 @@
 // let elem1 = document.querySelector("#text2")
 // console.log(elem1)
 
-let divs = document.querySelectorAll("h1")
+// let divs = document.querySelectorAll("h1")
 
-console.log(divs)
+// console.log(divs)
 
-divs.forEach((elem)=>{
-    console.log(elem)
-})
+// divs.forEach((elem)=>{
+//     console.log(elem)
+// })
+
+// let container = document.querySelector(".conatiner")
+
+// let heading = document.createElement("h1")
+// heading.textContent = "hello i am h1"
+// container.appendChild(heading)
+
+// let text2 = document.createElement("h2")
+// text2.innerText = "hello i am h2"
+// container.appendChild(text2)
+
+// let body = document.querySelector("body")
+
+// let text = document.querySelector("h1")
+
+// let p = document.createElement("p")
+// p.textContent = "hello i am p tag"
+// body.insertBefore(p, text)
+
+
+
+// let text = document.querySelector("h1")
+// text.remove()
+
+
+
+// let text = document.querySelector("h1")
+// // text.classList.add("test" , "hello")
+
+// text.classList.remove("text")
+
+// to get the parent element of current element
+
+// let text  = document.querySelector("h1")
+
+// // let value = text.parentNode
+
+// let privious = text.previousElementSibling
+
+// let next = text.nextElementSibling
+
+
+// // console.log(privious)
+
+// console.log(next)
+
+// console.log(value)
+
+// let box = document.querySelector(".box")
+
+// let elem = box.childNodes
+
+// // console.log(elem)
+
+
+// let first = box.firstElementChild
+// // console.log(first)
+
+// let last = box.lastElementChild
+
+// console.log(last)
+
+
+// let privious = box.previousElementSibling
+
+// console.log(privious)
+
+
+// let box = document.querySelector(".box")
+
+// let value = box.getAttribute("id")
+
+// console.log(value)
+
+
+// box.setAttribute("id" , "text")
+
+
+// box.removeAttribute("id")
+
+// box.removeAttribute("class")
+
+
+// function greet(){
+//     alert("heelo from om")
+// }
+
+
+// let btn = document.querySelector("button")
+// btn.onclick = greet
+
+
+
+// let btn = document.querySelector("div")
+
+// btn.addEventListener("click" , ()=>{
+
+//     btn.style.backgroundColor = "orange"
+//     btn.style.color ="white"
+//     btn.style.border = "none"
+// })
+
+
+// btn.addEventListener("dblclick", ()=>{
+//     alert("i am dblclick")
+// })
