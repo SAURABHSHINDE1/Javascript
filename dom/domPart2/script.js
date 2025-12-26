b9af4bdf74a8790e6eb085261ea9fb1190bca8c0
SAURABHSHINDE1 <saurabhshinde779845@gmail.com>
2025-12-26 13:01:44 +0530

    26 dec 2025

---
+++ dom/domPart2/script.js
@@ -0,0 +1,72 @@
+
+
+// let heading = document.querySelector(".text")
+// heading.textContent= "hello "
+
+
+// let heading = document.querySelector(".text")
+// heading.innerText= "hello "
+
+
+// let heading = document.querySelector(".text")
+// heading.innerHTML= "<p> hello to all </p>"
+
+
+// let tname = document.getElementById("text")
+// tname.textContent= "hello"
+
+// let box = document.querySelector(".box")
+// box.style.height = "400px"
+// box.style.width ="400px"
+// box.style.backgroundColor = "orange"
+// box.style.display ="flex"
+// box.style.alignItems ="center"
+// box.style.justifyContent ="center"
+// box.style.borderRadius ="20px"
+
+// let heading = document.createElement("h1")
+// heading.textContent = "Hello DreamsGuider !"
+// heading.style.color="red"
+// heading.style.textAlign = "center"
+// box.appendChild(heading)
+
+// let heading = document.getElementById("text")
+// console.log(heading)
+
+
+
+// let dives = document.getElementsByClassName("box")
+// console.log(dives)
+
+
+// let tags = document.getElementsByTagName("h1")
+// console.log(tags)
+
+
+// let tag = Array.from(tags)
+
+// console.log(tag)
+
+// tag.forEach((elem)=>{
+
+//     elem.style.color = "red"
+//     elem.style.fontSize ="40px"
+
+// })
+
+// let heading = document.querySelector("h1")
+// console.log(heading)
+
+// let elem = document.querySelector(".text")
+// console.log(elem)
+
+// let elem1 = document.querySelector("#text2")
+// console.log(elem1)
+
+let divs = document.querySelectorAll("h1")
+
+console.log(divs)
+
+divs.forEach((elem)=>{
+    console.log(elem)
+})
