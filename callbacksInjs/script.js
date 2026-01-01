493525aeac87ec8af4deda884923ccaf48513c0c
SAURABHSHINDE1 <saurabhshinde779845@gmail.com>
2026-01-01 18:01:27 +0530

    1 jan 2026

---
+++ callbacksInjs/script.js
@@ -0,0 +1,50 @@
+// function greet(id ,callback){
+
+//     console.log(id )
+
+//     callback();
+
+// }
+
+
+// greet(1 , ()=>{
+//     console.log("mala data bhtala")
+
+//     greet(2 , ()=>{
+//         console.log("mala parat data bhetala")
+
+//         greet(3 ,()=>{
+//             console.log("final data")
+
+//             greet(4 ,()=>{
+//                 console.log("after final")
+//             })
+//         })
+
+//     })
+// })
+
+
+
+// setTimeout(()=>{
+
+//     console.log("1st data recived")
+
+//     setTimeout(()=>{
+
+//         console.log("2nd data recived")
+
+//         setTimeout(()=>{
+
+//             console.log("3rd data recived")
+
+//             setTimeout(()=>{
+//                 console.log("4th data recived")
+//             },2000)
+
+//         },2000)
+
+//     },2000)
+
+// }, 2000)
+
--- promises/script.js
+++ promises/script.js
@@ -1,26 +1,2 @@
-// let promise = new Promise((resolve , reject)=>{
 
-//     let data = false
-
-//         setTimeout(()=>{
-
-//            if(data){
-//             console.log("data 111111")
-//             resolve("mla data bhetala !!!")
-//            }
-//            else{
-//             reject("mala data bhetala nahiiii")
-//            }
-
-//         },2000)
-
-// })
-
-// promise.then((res)=>{
-//     console.log(res)
-// })
-
-// promise.catch((err)=>{
-//     console.log("netwok err " + err)
-// })
 
